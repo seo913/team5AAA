456c00da9413691bb1ccded2088560ec742713c4
semkwon <3pkwon@gmail.com>
2023-06-28 13:46:16 +0900

    민팅 페이지

--- app/mint/mintform.js
+++ app/mint/mintform.js
@@ -1,14 +1,14 @@
-'use client';
-import { useState } from 'react';
-import axios from 'axios';
-import Web3 from 'web3';
-import { CONTRACT_ABI, CONTRACT_ADDRESS } from '@/pages/api/web3.config';
+"use client";
+import { useState } from "react";
+import axios from "axios";
+import Web3 from "web3";
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "@/pages/api/web3.config";
 
 const FileUpload = () => {
   const PINATA_JWT = process.env.NEXT_PUBLIC_PINATA_JWT;
   const [selectedFile, setSelectedFile] = useState();
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const [file, setFile] = useState(); // 미리 보여주는 이미지
   const [isCheckedNormal, setIsCheckedNormal] = useState(false);
   const [isCheckedTimeCapsule, setIsCheckedTimeCapsule] = useState(false);
@@ -35,53 +35,52 @@ const FileUpload = () => {
 
   const handleCheckboxChange = (event) => {
     const { name } = event.target;
-    if (name === 'normal') {
+    if (name === "normal") {
       setIsCheckedNormal(true);
       setIsCheckedTimeCapsule(false);
       setType(0);
-    } else if (name === 'timecapsule') {
+    } else if (name === "timecapsule") {
       setIsCheckedNormal(false);
       setIsCheckedTimeCapsule(true);
       setType(1);
     }
   };
 
-  const [selectedPeriod, setSelectedPeriod] = useState('week');
+  const [selectedPeriod, setSelectedPeriod] = useState("week");
 
   const handlePeriodChange = (event) => {
     setSelectedPeriod(event.target.value);
   };
 
   const handleSubmission = async () => {
-
     const accounts = await window.ethereum.request({
-      method: 'eth_requestAccounts',
+      method: "eth_requestAccounts",
     });
     const address = accounts[0];
     console.log(address);
 
     const formData = new FormData();
 
-    formData.append('file', selectedFile);
+    formData.append("file", selectedFile);
 
     const metadata = JSON.stringify({
-      name: 'file name',
+      name: "file name",
     });
-    formData.append('pinataMetadata', metadata);
+    formData.append("pinataMetadata", metadata);
 
     const options = JSON.stringify({
       cidVersion: 0,
     });
-    formData.append('pinataOptions', options);
+    formData.append("pinataOptions", options);
 
     try {
       const res = await axios.post(
-        'https://api.pinata.cloud/pinning/pinFileToIPFS',
+        "https://api.pinata.cloud/pinning/pinFileToIPFS",
         formData,
         {
           maxBodyLength: Infinity,
           headers: {
-            'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+            "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
             Authorization: `Bearer ${PINATA_JWT}`,
           },
         }
@@ -94,15 +93,15 @@ const FileUpload = () => {
         type === 0
           ? Math.floor(new Date().getTime() / 1000)
           : Math.floor(new Date().getTime() / 1000) +
-            (selectedPeriod === 'week'
+            (selectedPeriod === "week"
               ? 604800
-              : selectedPeriod === 'month'
+              : selectedPeriod === "month"
               ? 2592000
               : 31536000);
       // });
 
       const res2 = await axios.post(
-        'https://api.pinata.cloud/pinning/pinJSONToIPFS',
+        "https://api.pinata.cloud/pinning/pinJSONToIPFS",
         {
           name: title,
           description: [
@@ -128,105 +127,129 @@ const FileUpload = () => {
         )
         .send({ from: address });
       console.log(res2.data);
-      alert('민팅이 완료되었습니다.');
+      alert("민팅이 완료되었습니다.");
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <div className='flex gap-20'>
-      <div className='mb-4'>
-        <p className=' font-bold text-2xl '>추억 민팅하기</p>
-        <p>지원 가능한 파일형식(jpeg, jpg, webp, png, bmp)</p>
-        <input type='file' onChange={changeHandler} className='border mt-2' />
-        <div className='w-[350px] h-[200px]'>
+    <div className="flex gap-20">
+      <div className="p-4">
+        <div className="w-[450px] h-[450px] ">
           {file && (
             <img
               src={URL.createObjectURL(file)}
-              className=' w-[100%] h-[100%] mt-2'
+              className=" w-[100%] h-[100%] rounded-t-3xl"
             />
           )}
         </div>
+        <div className="bg-zinc-800 p-4 rounded-b-3xl">
+          <p className=" font-bold text-3xl text-slate-300">추억 민팅하기</p>
+          <p className="my-2 text-slate-200">
+            지원 가능한 파일형식(jpeg, jpg, webp, png, bmp)
+          </p>
+          <input
+            type="file"
+            onChange={changeHandler}
+            className="text-sm text-slate-200
+          file:mr-5 file:py-3 file:px-10
+          file:rounded-2xl file:border-0
+          file:text-md file:font-semibold  file:text-white
+          file:bg-gradient-to-r file:from-blue-600 file:to-purple-600
+          hover:file:cursor-pointer hover:file:opacity-80
+        "
+          />
+        </div>
       </div>
 
-      <div className='mb-4'>
-        <p className=' font-bold text-2xl '>제목</p>
-        <p>추억의 제목을 입력해주세요.</p>
-        <input
-          type='text'
-          value={title}
-          onChange={handleTitleChange}
-          className='border w-[400px] '
-        />
-
-        <p className=' font-bold text-2xl mt-2'>설명</p>
-        <p>추억에 대해 간략하게 소개해주세요.</p>
-        <input
-          type='text'
-          value={content}
-          onChange={handleContentChange}
-          className='border w-[400px]'
-        />
-
-        <p className=' font-bold text-2xl mt-2'>타입</p>
-        <p>원하는 타입을 선택해주세요.</p>
-        <span>
+      <div className="p-4 pt-16 ">
+        <div className="mb-5">
+          <p className=" font-bold text-3xl mb-2 font-mono text-white">TITLE</p>
           <input
-            type='checkbox'
-            name='normal'
-            value={type}
-            checked={isCheckedNormal}
-            onChange={handleCheckboxChange}
+            class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 px-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+            placeholder="추억의 제목을 입력해주세요."
+            type="text"
+            value={title}
+            onChange={handleTitleChange}
           />
-          일반
-        </span>
-        <span>
+        </div>
+
+        <div className="mb-5">
+          <p className=" font-bold text-3xl mb-2 font-mono text-white">DESCRIPTION</p>
           <input
-            type='checkbox'
-            name='timecapsule'
-            value={type}
-            checked={isCheckedTimeCapsule}
-            onChange={handleCheckboxChange}
+            class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 px-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+            placeholder="추억에 대해 간략하게 소개해주세요."
+            type="text"
+            value={content}
+            onChange={handleContentChange}
           />
-          타임캡슐
-        </span>
+        </div>
+
+        <div className="mb-5">
+          <p className=" font-bold text-3xl mb-2 font-mono text-white">REVEAL-TYPE</p>
+          <p>원하는 타입을 선택해주세요.</p>
+          <span>
+            <input
+              type="checkbox"
+              name="normal"
+              value={type}
+              checked={isCheckedNormal}
+              onChange={handleCheckboxChange}
+            />
+            일반
+          </span>
+          <span>
+            <input
+              type="checkbox"
+              name="timecapsule"
+              value={type}
+              checked={isCheckedTimeCapsule}
+              onChange={handleCheckboxChange}
+            />
+            타임캡슐
+          </span>
+        </div>
 
         {isCheckedTimeCapsule && (
-          <div className='mb-4'>
-            <p className='font-bold text-2xl'>기간</p>
+          <div className="mb-5">
+            <p className="font-bold text-3xl mb-2 font-mono text-white">DURATION</p>
             <p>원하는 기간을 선택해주세요.</p>
             <input
-              type='radio'
-              name='timestamp'
-              value='week'
-              checked={selectedPeriod === 'week'}
+              type="radio"
+              name="timestamp"
+              value="week"
+              checked={selectedPeriod === "week"}
               onChange={handlePeriodChange}
             />
             7일
             <input
-              type='radio'
-              name='timestamp'
-              value='month'
-              checked={selectedPeriod === 'month'}
+              type="radio"
+              name="timestamp"
+              value="month"
+              checked={selectedPeriod === "month"}
               onChange={handlePeriodChange}
             />
             30일
             <input
-              type='radio'
-              name='timestamp'
-              value='year'
-              checked={selectedPeriod === 'year'}
+              type="radio"
+              name="timestamp"
+              value="year"
+              checked={selectedPeriod === "year"}
               onChange={handlePeriodChange}
             />
             365일
           </div>
         )}
 
-        <div className='mt-4'>
+        <div>
           <button
             onClick={handleSubmission}
-            className=' bg-slate-300 w-[400px] hover:bg-violet-200 rounded-md shadow-md shadow-indigo-400 font-semibold'
+            className=" w-[400px] mr-5 py-3 px-10
+            rounded-2xl border-0
+            text-md font-semibold  text-white
+            bg-gradient-to-r from-blue-600 to-purple-600
+            hover:cursor-pointer hover:opacity-80"
           >
             추억민팅하기
           </button>
--- app/mint/page.js
+++ app/mint/page.js
@@ -3,9 +3,9 @@ import FileUpload from "./mintform";
 
 export default async function Mint() {
   return (
-    <div className='min-h-screen flex justify-center items-center '>
-        <FileUpload/>
-        {/* <LoginForm/> */}
+    <div className="min-h-screen flex justify-center items-center bg-gradient-to-br from-purple-700 via-indigo-700 to-pink-700">
+      <FileUpload />
+      {/* <LoginForm/> */}
     </div>
   );
 }
