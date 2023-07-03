26c2ad28d13b78355140e72ce6efbe89cb7294f2
seojaemin <seojaemin@seojaemin-ui-MacBookPro.local>
2023-07-03 15:29:52 +0900

    detailpage

--- app/all/allform.js
+++ app/all/allform.js
@@ -5,12 +5,13 @@ import { useEffect, useState } from 'react';
 import Web3 from 'web3';
 import axios from 'axios';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 
 export default function All() {
   const web3 = new Web3(window.ethereum);
   const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+  console.log(contract);
   const [metadataList, setMetadataList] = useState([]);
+  const [tokenIds, setTokenIds] = useState([]);
 
   useEffect(() => {
     fetchAllNFTs();
@@ -20,7 +21,7 @@ export default function All() {
     try {
       const totalSupply = await contract.methods.totalSupply().call();
       // console.log(totalSupply);
-      const startFromIndex = BigInt(11);
+      const startFromIndex = BigInt(0);
       // console.log(startFromIndex);
 
       const endAtIndex = BigInt(totalSupply) - BigInt(1);
@@ -32,25 +33,25 @@ export default function All() {
         // console.log(id);
         ids.push(id);
       }
+
+      setTokenIds(ids);
       // console.log(ids);
 
       const metadataList = await Promise.all(
         ids.map(async (id) => {
           const tokenURI = await contract.methods.tokenURI(id).call();
           const response = await axios.get(tokenURI);
-          console.log(response);
+
+          // console.log(response);
           return response.data;
         })
       );
-      // const filteredMetadataList = metadataList.filter((metadata) => metadata);
-      // setMetadataList(filteredMetadataList);
-      setMetadataList(metadataList);
+      const filteredMetadataList = metadataList.filter((metadata) => metadata);
+      setMetadataList(filteredMetadataList);
+      // setMetadataList(metadataList);
 
-      console.log(metadataList);
+      // console.log(metadataList);
       // console.log(metadataList[0]);
-
-      // console.log(metadataList[1].description[0].type);
-      // console.log(metadataList[11].url);
     } catch (error) {
       console.error(error);
     }
@@ -61,20 +62,24 @@ export default function All() {
       <div className='min-h-screen border '>
         <div className='flex gap-5 ml-10 mt-20'>
           <div className='border flex border-black pb-5 '>
-            {metadataList
-              .filter((metadata) => metadata.description[0].type === 0) // description type이 0인 것만 필터링 0은 일반
-              .map((metadata, index) => (
-                <div key={index} className='w-60 h-60  '>
-                  <Link href={`/detail/${metadataList[index].url}`}>
-                    <img
-                      src={metadata.image}
-                      alt='NFT'
-                      className='w-60 h-52 gap-10'
-                    />
-                  </Link>
-                  <p className='font-semibold p-1'>제목 : {metadata.name}</p>
-                </div>
-              ))}
+            {tokenIds &&
+              metadataList
+                .filter(
+                  (metadata) =>
+                    Array.isArray(metadata.description) && metadata.type === 0
+                )
+                .map((metadata, index) => (
+                  <div key={index} className='w-60 h-60  '>
+                    <Link href={`/detail/${tokenIds[index]}`}>
+                      <img
+                        src={metadataList[index].image}
+                        alt='NFT'
+                        className='w-60 h-52 gap-10'
+                      />
+                    </Link>
+                    <p className='font-semibold p-1'>제목 : {metadata.name}</p>
+                  </div>
+                ))}
           </div>
         </div>
       </div>
